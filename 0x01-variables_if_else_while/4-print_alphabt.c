cbb4c23139349b9710815d57a7bb7822d5167788
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:19:51 +0300

    filtering

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 97;
+    while (n < 123)
+    {
+        if (((char)n != 'e') && ((char)n != 'q'))
+        putchar((char)n);
+        
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
