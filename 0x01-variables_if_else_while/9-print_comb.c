5738292eb7a74ff9b0b435ca5a82527b3304de7a
Danielyilma <yilmad752@gmail.com>
2023-08-11 18:07:20 +0300

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 48;
+    while (n < 58)
+    {
+        putchar((char)n);
+        if (n!=57)
+        {
+         putchar(',');
+         putchar(' ');
+        }
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
