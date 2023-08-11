676e6652a7825ea86e031484564777df9e282b56
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:55:08 +0300

    hexa decimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 48;
+    while (n < 103)
+    {
+        putchar((char)n);
+        if (n == 57)
+        n+=39;
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
