653f56bdd955d000bb3caf278ae08549eabbb3ca
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:03:41 +0300

    loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 97;
+    while (n < 123)
+    {
+        putchar((char)n);
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
