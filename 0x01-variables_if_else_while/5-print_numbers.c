5ce56e497ab5ace707aa9c9e49fadd8d03d407e9
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:23:21 +0300

    loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 0;
+    while (n < 10)
+    {
+        printf("%d", n);
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
