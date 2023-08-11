799f9fa8ef4f4647d9d236c64950e36e97ed14a8
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:10:49 +0300

    upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 97, u = 65;
+    while (n < 123)
+    {
+        putchar((char)n);
+        n++;
+    }
+
+    while (u < 91)
+    {
+        putchar((char)u);
+        u++;
+    }
+
+    putchar('\n');
+    return (0);
+}
