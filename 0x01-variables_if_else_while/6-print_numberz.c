cd16d24b530c3fc77002e9400219725184ac5452
Danielyilma <yilmad752@gmail.com>
2023-08-11 17:29:37 +0300

    ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry to program
+ * Return: 0
+*/
+int main(void)
+{
+    int n  = 48;
+    while (n < 58)
+    {
+        putchar((char)n);
+        n++;
+    }
+    putchar('\n');
+    return (0);
+}
