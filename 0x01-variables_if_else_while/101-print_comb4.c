311ce96b74e8b9e526e058650a0be7a5cb077046
Danielyilma <yilmad752@gmail.com>
2023-08-11 19:02:18 +0300

    11 combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entery of the progarm
+ * Return: 0
+*/
+int main(void)
+{
+    int i = 48, j = 48, k = 48;
+
+    while (i < 58)
+    {
+        j = i + 1;  
+        k = j +1;
+        while (j < 58)
+        {
+            while (k < 58)
+            {
+              putchar((char)i);
+              putchar((char)j);
+              putchar((char)k);
+              if(i != 55)
+              {
+               putchar(',');
+               putchar(' ');
+              }
+              k++;
+            }
+            j++;
+        }
+        i++;
+    }
+    putchar('\n');
+
+}
