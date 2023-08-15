0b75bc5b0f589fb215181978597c39095358c6cf
Danielyilma <yilmad752@gmail.com>
2023-08-15 18:20:08 +0300

    play with digits

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,16 @@ void jack_bauer(void)
 {
 	int i, j, k, z;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 50; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = 48; j < 51; j++)
 		{
 			for (k = 48; k < 58; k++)
 			{
 				for (z = 48; z < 58; z++)
 				{
+					if (i == 2 && j == 3 && k == 5 && z == 9)
+					break;
 					_putchar((char)i);
 					_putchar((char)j);
 					_putchar(':');
@@ -25,6 +27,8 @@ void jack_bauer(void)
 					_putchar((char)z);
 					_putchar('\n');
 				}
+				if (i == 2 && j == 3 && k == 5)
+				break;
 			}
 		}
 	}
