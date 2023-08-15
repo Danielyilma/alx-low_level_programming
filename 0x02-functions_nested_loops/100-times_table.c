1982e6312530c3569c154cacdf013fe79294e15f
Danielyilma <yilmad752@gmail.com>
2023-08-15 23:50:17 +0300

    print multiple table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,21 +18,23 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				if (result > 9 && result < 99)
+				if (result > 9 && result < 100)
 				{
+					_putchar(' ');
 					_putchar((result / 10 + '0'));
 					_putchar((result % 10 + '0'));
 				}
 				else if (result > 99)
 				{
 					_putchar((result / 100 + '0'));
-					_putchar((result % 100) % 10 + '0');
+					_putchar((result % 100) / 10 + '0');
 					_putchar((result % 10 + '0'));
 				}
 				else
 				{
 					if (j != 0)
 					{
+					_putchar(' ');
 					_putchar(' ');
 					}
 					_putchar((result + '0'));
