f60a075ab19574a40fb65df783748e3a453336c3
Danielyilma <yilmad752@gmail.com>
2023-08-15 17:10:27 +0300

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,20 +3,21 @@
 /**
  * print_alphabet - print alphabet from start to end 10 times
  * @void: no parameter
- * Return: void
+ * Return: none
  */
 
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 97; j < 113; i++)
+		for (j = 97; j < 113; j++)
 		{
 			putchar((char)j);
 		}
 
 		putchar('\n');
 	}
-
+	return;
 }
