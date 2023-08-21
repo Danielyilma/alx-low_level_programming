86833cf2449f53d9ff77bf3fdeb381345a76bc8f
Danielyilma <yilmad752@gmail.com>
2023-08-21 17:30:25 +0300

    printing others

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,16 @@
 
 void puts2(char *str)
 {
+	int len = 0;
+
 	while (*str != '\0')
 	{
-		if (((int)*str - 48) % 2 == 0)
+		if (len % 2 == 0)
 		{
 			_putchar(*str);
 		}
 		str++;
+		len++;
 	}
 	_putchar('\n');
 }
