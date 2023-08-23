55819c66d53ab5426a71eee3541854dd0625c443
Danielyilma <yilmad752@gmail.com>
2023-08-23 19:07:40 +0300

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - concatinate string
+*
+* @s: pointer to character
+*
+* Return: integer
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+* _strncpy - concatinate n string
+*
+* @dest: pointer to character
+*
+* @src: pointer to character
+*
+* @n: number of integer to be concatinate
+*
+* Return: character pointer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	char *temp = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*temp = *src;
+		src++;
+		temp++;
+	}
+	temp -= n;
+
+	return (temp);
+}
