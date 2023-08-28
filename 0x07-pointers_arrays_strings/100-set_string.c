216bc997dfd3f87623ec104b0f77b6602745c4a7
Danielyilma <yilmad752@gmail.com>
2023-08-28 23:25:06 +0300

    password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - filles same n
+ *
+ * @s: character pointer
+ * @to: character
+ *
+ * Return: character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
