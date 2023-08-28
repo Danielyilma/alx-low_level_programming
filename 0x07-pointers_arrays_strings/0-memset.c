a7cc270ac432bf44761248eefe014f6d4dc26540
Danielyilma <yilmad752@gmail.com>
2023-08-28 15:45:55 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - filles same n 
+ *
+ * @s: character pointer
+ * @b: character
+ * @n: integer
+ * 
+ * Return: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < (int)n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
