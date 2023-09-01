b561e61b60d823971d2334fce3c7dfb3f380285f
Danielyilma <yilmad752@gmail.com>
2023-09-01 21:41:53 +0300

    printing number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print command line argument
+ *
+ * @argc: integer
+ *
+ * @argv: array of pointer
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
