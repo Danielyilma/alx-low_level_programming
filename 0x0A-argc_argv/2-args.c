43656d6bee0676fccfcefda68603998292294af0
Danielyilma <yilmad752@gmail.com>
2023-09-01 21:48:34 +0300

    printing arguments in standard output

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print command line argument
+ *
+ * @argc: integer
+ *
+ * @argv: array of pointer
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
