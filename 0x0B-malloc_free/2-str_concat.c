22e3cc59c71e3e07554c8158ad3659c41f462ece
Danielyilma <yilmad752@gmail.com>
2023-09-04 17:13:31 +0300

    concatinate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - creates array and initialize
+ *
+ * @s1: character pointer
+ *
+ * @s2: character pointer
+ *
+ * Return: character pointer
+ *
+ **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	char *array;
+
+	array = (char *)malloc(sizeof(s1) + sizeof(s2) - 1);
+
+	if (array == NULL)
+	return (NULL);
+	while (s1[i] != '\0')
+	{
+		array[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		array[i] = s2[j];
+		j++;
+		i++;
+	}
+	return (array);
+}
