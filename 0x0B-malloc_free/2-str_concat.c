00745c9c75495ed1321b1473c472e8a3dbf85f14
Danielyilma <yilmad752@gmail.com>
2023-09-05 09:18:12 +0300

    concatinate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,26 @@
 #include "main.h"
 
 /**
+* _strlen - changes integer to 98
+*
+* @s: pointer to character array
+*
+* Return: integer
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len + 1);
+}
+
+
+/**
  * str_concat - creates array and initialize
  *
  * @s1: character pointer
@@ -15,10 +35,13 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, len1, len2;
 	char *array;
 
-	array = (char *)malloc(sizeof(*s1) + sizeof(*s2) - 1);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	array = (char *)malloc(sizeof(char) * (len1 + len2));
 
 	if (array == NULL)
 	return (NULL);
