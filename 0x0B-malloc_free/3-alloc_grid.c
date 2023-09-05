9b1369da8e5d1b36a10a1f14d1cf731789417e26
Danielyilma <yilmad752@gmail.com>
2023-09-05 09:50:48 +0300

    2 d array in heap

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates array and initialize
+ *
+ * @width: int
+ *
+ * @height: int
+ *
+ * Return: int pointer to pointer
+ *
+ **/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, k, **array;
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+
+	array = (int **)malloc(sizeof(int *) * height);
+
+	if (array == NULL)
+	return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		return (NULL);
+	}
+
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+		{
+			array[j][k] = 0;
+		}
+	}
+	return (array);
+}
