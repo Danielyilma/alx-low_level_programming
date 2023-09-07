03198d63fc208c1962118f6a9317eaba217a5fc1
Danielyilma <yilmad752@gmail.com>
2023-09-07 14:12:12 +0300

    array range

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,6 +36,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		nptr = malloc(old_size + new_size);
+		if (!nptr)
+		return (NULL);
 		for (i = 0; i < old_size; i++)
 		{
 			nptr[i] = ((char *)ptr)[i];
