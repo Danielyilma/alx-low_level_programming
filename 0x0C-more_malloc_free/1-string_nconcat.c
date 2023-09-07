f8d7849cfe46bca3c7c72015311aa13dff1ff3c9
Danielyilma <yilmad752@gmail.com>
2023-09-07 13:39:40 +0300

    concatinate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = _strlen(s2);
 
 	if (s1 == NULL)
-	s1 = "";
+	s1 = '\0';
 	if (s2 == NULL)
-	s2 = "";
+	s2 = '\0';
 
 	if (n >= len2)
 	n = len2;
