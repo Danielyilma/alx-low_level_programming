1813ecfa9c1dd0d00a5ae4419a8985564090cef9
Danielyilma <yilmad752@gmail.com>
2023-09-11 17:35:21 +0300

    freeing struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - initialize dog description
+ *
+ * @d: struct dog
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d);
+{
+	free(d);
+}
