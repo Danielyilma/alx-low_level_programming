33554ea722d4207f0820ffa247dc9f835bca99b0
Danielyilma <yilmad752@gmail.com>
2023-09-13 15:09:26 +0300

    opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,32 @@
  * Return: 0
  */
 
- int main(int argc, char *argv[])
- {
-	
+int main(int argc, char *argv[])
+{
+	int i, bites;
+	char *pointer;
 
- }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+
+	bites = atoi(argv[1]);
+	pointer = (char *)main;
+
+	if (bites < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bites; i++)
+	{
+		printf("%02hhx", pointer[i]);
+		if (i != (bites - 1))
+		printf(" ");
+	}
+	printf("\n");
+}
