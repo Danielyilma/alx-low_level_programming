02e4838561c7e8c743225c0872de292e4b729f33
Danielyilma <yilmad752@gmail.com>
2023-09-14 20:28:03 +0300

    printing variable string

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,53 +3,69 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum all parameter
+ * _switch - sum all parameter
  *
- * @n: size
+ * @_format: size
+ * @in: bool
+ * @arg1: va_list
+ *
+ * Return: nothing
+ */
+
+void _switch(char _format, int *in, va_list arg1)
+{
+	int num;
+	char ch, *chr;
+	float numf;
+
+	switch (_format)
+		{
+			case 'c':
+			ch = va_arg(arg1, int);
+			printf("%c", ch);
+			*in = 1;
+			break;
+			case 'i':
+			num = va_arg(arg1, int);
+			printf("%d", num);
+			*in = 1;
+			break;
+			case 'f':
+			numf = va_arg(arg1, double);
+			printf("%f", numf);
+			*in = 1;
+			break;
+			case 's':
+			chr = va_arg(arg1, char *);
+			if (chr == NULL)
+			{
+				printf("(nil)");
+				*in = 1;
+				break;
+			}
+			*in = 1;
+			printf("%s", chr);
+			break;
+		}
+}
+/**
+ * print_all - sum all parameter
+ *
+ * @format: size
  *
  * Return: integer
  */
 
 void print_all(const char * const format, ...)
 {
-	int j = 0, num;
-	char ch, *chr;
-	float numf;
-	int inswitch = 0;
+	int j = 0, inswitch = 0;
+
 	va_list args;
 
 	va_start(args, format);
 	while (format[j])
 	{
-		switch (format[j])
-		{
-			case 'c':
-			ch = va_arg(args, int);
-			printf("%c", ch);
-			inswitch = 1 ;
-			break;
-			case 'i':
-			num = va_arg(args, int);
-			printf("%d", num);
-			inswitch = 1;
-			break;
-			case 'f':
-			numf = va_arg(args, double);
-			printf("%f", numf);
-			inswitch = 1;
-			break;
-			case 's':
-			chr = va_arg(args, char *);
-			if (chr == NULL)
-			{
-				printf("(nil)");
-				inswitch = 1;
-				break;
-			}
-			inswitch = 1;
-			printf("%s", chr);
-			break;			
-		}
+		_switch(format[j], &inswitch, args);
 		if (format[j + 1] != '\0' && inswitch)
 		{
 			printf(", ");
