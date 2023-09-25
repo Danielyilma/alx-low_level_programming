62a2e3c4dc5c6ddf4e50ab1cccdb50e3ddcdaff4
Danielyilma <yilmad752@gmail.com>
2023-09-26 02:10:02 +0300

    more singly linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverses a list
  *
+ * @head: - head of a list
  *
- *
+ * Return: head of the reverse list
  */
 
 listint_t *reverse_listint(listint_t **head)
