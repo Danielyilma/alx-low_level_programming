2a825ae6af84a654f904319dc2c9bc6ee223796b
Danielyilma <yilmad752@gmail.com>
2023-09-26 03:11:44 +0300

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	int len = 0, check;
+
+	if (*h == NULL)
+	return (0);
+
+
+	while (h != NULL)
+	{
+		check = *h - ((*h)->next);
+		if (check > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
