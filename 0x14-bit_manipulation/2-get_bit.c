fe5ed7095b3cbc2afe389ebdfe62640a8bf2bc06
Danielyilma <yilmad752@gmail.com>
2023-09-29 20:17:36 +0300

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * twos_power - fin power of 2's
- *
- * @pow: power for the base
- *
- * Return: integer
- *
- */
-
-int twos_power(int pow)
-{
-	int num = 1;
-
-	while (pow > 0)
-	{
-		num *= 2;
-		pow--;
-	}
-	return (num);
-}
-
-/**
  * get_bit - get specefic bit from binary
  *
  * @n: decimal number
@@ -33,9 +12,6 @@ int twos_power(int pow)
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int value = -1;
-
-	if ((int)n < twos_power(index))
-	return (-1);
 
 	n = n >> index;
 	value = n & 1;
