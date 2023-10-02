bf439194bc03f463a48a6cea87ff7afc2b2851da
Danielyilma <yilmad752@gmail.com>
2023-10-02 23:46:37 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	create = open(filename, O_CREAT | O_WRONLY | permission);
+	create = open(filename, O_CREAT | O_WRONLY | O_TRUNC| permission);
 
 	while (text_content[i] != 0)
 	i++;
