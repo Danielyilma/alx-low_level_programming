70d2cbdea855c036b590bb8b5e1acc50189e3c2d
Danielyilma <yilmad752@gmail.com>
2023-10-02 22:32:02 +0300

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL || fd == -1)
 	return (-1);
 
+	if (text_content == NULL)
+	return (1);
+
 	while (text_content[i] != '\0')
 	i++;
 
