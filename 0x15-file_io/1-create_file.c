dd736a73852215a8b55d5220258e1e028d5e63f9
Danielyilma <yilmad752@gmail.com>
2023-10-02 22:17:31 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,29 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        int i = 0, num = 0, create = open(filename, O_WRONLY);
-        mode_t permission = S_IRUSR | S_IWUSR;
+	int i = 0, num = 0, create = open(filename, O_WRONLY);
+	mode_t permission = S_IRUSR | S_IWUSR;
 
-        if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+	return (-1);
 
-        if (create == -1)
-        {
-                close(create);
-                create = open(filename, O_CREAT | O_WRONLY | permission);
-                if (create == -1)
-                return (-1);
-        }
+	if (create == -1)
+	{
+		close(create);
+		create = open(filename, O_CREAT | O_WRONLY | permission);
+		if (create == -1)
+		return (-1);
+	}
 
-        while (text_content[i] != 0)
-        i++;
+	while (text_content[i] != 0)
+	i++;
 
-        num = write(create, text_content, i);
+	num = write(create, text_content, i);
 
-        if (num == 0 && i != 0)
-        return (-1);
+	if (num == 0 && i != 0)
+	return (-1);
 
-        close(create);
+	close(create);
 
-        return (1);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
+ * append_text_to_file - appending a text
  *
+ * @filename: the file name
+ * @text_content:
  *
- *
- *
+ * Return: integer
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int i = 0, fd = open(filename, O_APPEND);
+	int i = 0, fd = open(filename, O_WRONLY | O_APPEND);
 
-        if (!filename || fd == -1)
-        return (-1);
+	if (filename == NULL || fd == -1)
+	return (-1);
 
-        while (text_content[i] != '\0')
-        i++;
+	while (text_content[i] != '\0')
+	i++;
 
-        write(fd, text_content, 1);
+	write(fd, text_content, i);
+	close(fd);
 
-        return (1);
+	return (1);
 }
