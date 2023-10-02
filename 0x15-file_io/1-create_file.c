64fce361e1c06f685e36584517affe4a7c595f52
Danielyilma <yilmad752@gmail.com>
2023-10-02 23:52:44 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	create = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permission);
 
-	while (text_content[i] != 0)
+	while (text_content[i] != '\0')
 	i++;
 
 	num = write(create, text_content, i);
