4344b591ffb477b0572d64a5a9625a8adb96ccf7
Danielyilma <yilmad752@gmail.com>
2023-10-02 23:44:25 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,26 +11,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, num = 0, create = open(filename, O_WRONLY);
+	int i = 0, num = 0, create;
 	mode_t permission = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 	return (-1);
 
-	if (create == -1)
-	{
-		close(create);
-		create = open(filename, O_CREAT | O_WRONLY | permission);
-		if (create == -1)
-		return (-1);
-	}
+	create = open(filename, O_CREAT | O_WRONLY | permission);
 
 	while (text_content[i] != 0)
 	i++;
 
 	num = write(create, text_content, i);
 
-	if (num == 0 && i != 0)
+	if (num == -1 || create == -1)
 	return (-1);
 
 	close(create);
