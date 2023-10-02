81bb8ff20351863052e88837ae49bfe59b6ffd4f
Danielyilma <yilmad752@gmail.com>
2023-10-02 22:01:02 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,37 @@
 #include "main.h"
 
 /**
+ * create_file - creating a file
  *
+ * @filename: string
+ * @text_content: text to be written
  *
- *
- *
- *
+ * Return: integer
  */
 
 int create_file(const char *filename, char *text_content)
 {
-        
+        int i = 0, num = 0, create = open(filename, O_WRONLY);
+        mode_t permission = S_IRUSR | S_IWUSR;
+
+        if (filename == NULL)
+        return (-1);
+
+        if (create == -1)
+        {
+                close(create);
+                create = open(filename, O_CREAT | permission);
+                if (create == -1)
+                return (-1);
+        }
+
+        while (text_content[i] != 0)
+        i++;
+
+        num = write(create, text_content, i);
+
+        if (num == 0 && i != 0)
+        return (-1);
+
+        return (1);
 }
--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -1,52 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - check the code .
- *
- * @ac: Arguments counter
- * @av: Arguments array
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-	ssize_t n;
-	size_t size;
-	char *file;
-	int fd1, fd2;
+    int res;
 
-	if (ac != 3)
-	{
-		printf("Usage: %s filename size_to_read\n", av[0]);
-		exit(1);
-	}
-	fd1 = open("main.c", O_RDONLY);
-	if (fd1 == -1)
-	{
-		printf("Can't open file.\n");
-		return (1);
-	}
-	file = av[1];
-	if (strcmp(file, "NULL") == 0)
-		file = NULL;
-	size = atoi(av[2]);
-	n = read_textfile(av[1], size);
-	printf("\n(printed chars: %li)\n", n);
-	fd2 = open("0-read_textfile.c", O_RDONLY);
-	if (fd2 == -1)
-	{
-		printf("Can't open file.\n");
-		return (1);
-	}
-	if (fd2 != fd1 + 1)
-	{
-		printf("Stream was not closed !\n");
-		return (1);
-	}
-	return (0);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
