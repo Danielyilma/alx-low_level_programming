680926802edfbed5ffbc7df76a588d43cc54e067
Danielyilma <yilmad752@gmail.com>
2023-12-01 17:52:24 +0300

    implementing hashmap

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,11 +11,13 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t **current = ht->array, *temp = NULL;
+	hash_node_t **current, *temp = NULL;
 	unsigned int i = 0, check_first = 0;
 
 	if (ht == NULL)
 	return;
+
+	current = ht->array;
 
 	while (i < ht->size)
 	{
