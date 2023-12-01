65fd21c5e4da4b065ff57267024bf20ea15182c7
Danielyilma <yilmad752@gmail.com>
2023-12-01 17:40:34 +0300

    implementing hashmap

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -29,7 +29,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (check_first)
 				printf(", ");
-				printf("'%s': '%s'", temp->key, temp->value);
+				printf("'%s':'%s'", temp->key, temp->value);
 				temp = temp->next;
 				check_first = 1;
 			}
